50fa06fc0ed6061dba2e0b5549a86ec821d1a2f2
Syed Hussain Abbas <syed.hussain.whgs@gmail.com>
2023-06-07 22:28:58 +0300

    ejs module posting data through post api via form getting data through get api

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const http = require("http")
-const { name, secondName  } = require("./features")
+const express = require("express");
+const path = require("path");
 
-const server = http.createServer((req,res)=>{
-    if(req.url=='/'){
-        res.end(`<h1>Home ${name} ${secondName}</h1>`)
-    }else if(req.url=='/contact'){
-        res.end("<h1>Contact</h1>")
-    }else{
-        res.end("<h1>Served</h1>")
-    }
+const app = express();
+
+const users = []
+
+//Middlewares
+app.use(express.urlencoded({ extended: true }));
+
+//Engine
+app.set("view engine", "ejs");
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index", { heading: 'Home' });
+});
+
+app.post("/form-submit", (req, res) => {
+  console.log(req.body);
+  users.push({
+    name:req.body.name,
+    email:req.body.email
+  })
+  res.render('success')
+});
+
+app.get('/getUsers',(req,res)=>{
+    res.json({
+        users
+    })
 })
 
-server.listen(3000,()=>{
-    console.log("served")
-})
+app.get("/getProducts", (req, res) => {
+  const pathLocation = path.resolve();
+  // console.log(pathLocation)
+  // console.log(path.join(pathLocation,'/index.html'))
+  res.sendFile(path.join(pathLocation, "/index.html"));
+});
+
+//Server Listenser
+app.listen(3000, () => {
+  console.log("server is listening");
+});

2c98e2d01d0271229d242a102de6fd6a6724868d
Syed Hussain Abbas <syed.hussain.whgs@gmail.com>
2023-07-05 03:12:59 +0500

    authenticate middleware implemented

---
+++ middlewares/authenticate.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+const sendResponse = require("../helpers/sharedHelpers")
+
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (authorization) {
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, decoded) => {
+      if (err) {
+        console.log("err=> ", err);
+        sendResponse(res, 400, {
+          message: "Token rejected",
+        });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    sendResponse(res, 400, {
+      message: "Unauthorized",
+    });
+  }
+};
+
+module.exports = authenticate;
--- routes/travelPackages/packageRoutes.js
+++ routes/travelPackages/packageRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const {
   postPackageController,
 } = require("../../controllers/packageControllers");
+const authenticate = require("../../middlewares/authenticate");
 
-router.post(`/post-package`, postPackageController);
+router.post(`/post-package`, authenticate, postPackageController);
 
 module.exports = router;

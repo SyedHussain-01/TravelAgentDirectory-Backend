42696c5da235222528e7c2b66c4213426c3d9b5a
Syed Hussain Abbas <syed.hussain.whgs@gmail.com>
2023-07-07 23:27:58 +0500

    Revert "Revert "token verification flow fixed""
    This reverts commit d2649136ae1b0dc6f5965667898ff6256d008e5c.

--- helpers/authHelpers.js
+++ helpers/authHelpers.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const sendResponse = require("./sharedHelpers");
 
 //Functions
 
 const getAccessToken = (ss) => {
+  console.log("ss at=> ", ss)
   const token = jwt.sign(
-    { id: ss._id, name: ss.name, user_type:ss.user_type },
+    { _id: ss._id, name: ss.name, user_type: ss.user_type },
     `${process.env.JWT_TOKEN_KEY}`,
     { expiresIn: "15m" }
   );
@@ -14,37 +14,37 @@ const getAccessToken = (ss) => {
 };
 
 const getRefreshToken = (ss) => {
+  console.log("ss rt=> ", ss)
   const token = jwt.sign(
-    { id: ss._id, name: ss.name, user_type:ss.user_type },
+    { _id: ss._id, name: ss.name, user_type: ss.user_type },
     `${process.env.JWT_REFRESH_TOKEN_KEY}`,
     { expiresIn: "7d" }
   );
   return token;
 };
 
-
-const encryptPassword = async (pass) => {
-  const saltRounds = process.env.HASH_ROUNDS
+const encrypt = async (pass) => {
+  const saltRounds = Number(process.env.HASH_ROUNDS);
   try {
-    const hashedPass = await bcrypt.hash(pass, saltRounds)
+    const hashedPass = await bcrypt.hash(pass, saltRounds);
     return hashedPass;
   } catch (error) {
-    console.log("err=> ", error)
+    console.log("err=> ", error);
   }
-}
+};
 
-const decryptPassword = async (userPass, hashedPass) => {
+const decrypt = async (userPass, hashedPass) => {
   try {
-    const verify = await bcrypt.compare(userPass, hashedPass)
-    return verify; 
+    const verify = await bcrypt.compare(userPass, hashedPass);
+    return verify;
   } catch (error) {
-    console.log("error in verification=> ", error)
+    console.log("error in verification=> ", error);
   }
-}
+};
 
 module.exports = {
   getAccessToken,
   getRefreshToken,
-  encryptPassword,
-  decryptPassword
+  encrypt,
+  decrypt,
 };
